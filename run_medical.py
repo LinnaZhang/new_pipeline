fd6bd1d920a522f709092b131ab922808b07e3d9
zhangzl <zhangzl218@163.com>
2026-06-30 14:57:55 +0800

    执行文件补充

--- run_medical.py
+++ run_medical.py
@@ -7,6 +7,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from core_engine.action_dispatcher import PipelineEngine
 
 if __name__ == "__main__":
-    config_path = os.path.join("configs", "db_public_env.yaml")
+    config_path = os.path.join("configs", "db_medical.yaml")
     engine = PipelineEngine(config_path)
     engine.run()
